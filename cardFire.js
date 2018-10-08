66ec67174571d374fb1e04307be61c00ecc46672
cKeinsley <keinsleyChris@gmail.com>
2018-10-08 14:57:06 -0400

    Refactored to use async waterfall and added extra error handling

--- cardFire.js
+++ cardFire.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const RosefireTokenVerifier = require('rosefire-node')
 const redis = require('redis')
+const async = require('async')
 const fs = require('fs')
 
 const USER_TO_CARD = 'UserToCard'
@@ -21,56 +22,68 @@ const port = 3001
 const jsonParser = bodyParser.json()
 
 app.get('/test', (req, res) => {
-    res.send(client.PING())
+    client.PING((err, response) => {
+        if (err) {
+            res.send('Cannot PING')
+        }
+        res.send(response)
+    })
+
 })
 
 /**
  * Set contentType header to application/json
  */
 app.post('/token', jsonParser, (req, res) => {
-    if (!req.body) return res.sendStatus(400)
-    let cardNumber = req.body.cardNumber
-    let roseFireToken = req.get('RoseFireToken')
-    if (roseFireToken) {
-        try {
-            let username = validateRoseFireToken(roseFireToken, cardNumber, removeOldCard)
-            addNewCard(username, cardNumber)
-        } catch (err) {
-            // TODO: Handle different error types
-            res.status(401).json({
-                error: 'Not authorized'
-            })
-            return
-        }
+    if (!req.body) {
+        return res.sendStatus(400)
     }
-    res.status(200).json({
-        'success': true
-    })
+
+    let cardNumber = req.body.cardNumber
+    let rosefireToken = req.get('RosefireToken')
+
+    if (rosefireToken && cardNumber) {
+        async.waterfall([
+            validateRoseFireToken(rosefireToken, cardNumber),
+            removeOldCard,
+            addNewCard,
+        ], function(err, result) {
+            console.error(err)
+            if (err) {
+                res.status(401).json({ error: 'Not authorized' })
+            } else {
+                res.status(200).json({ 'success': true })
+            }
+            return
+        })
+    } else {
+        res.status(400).json({ 'message': 'You must include a RosefireToken header and cardNumber in the body' })
+    }
 })
 
-function validateRoseFireToken (roseFireToken, cardNumber, callback) {
-    rosefire.verify(roseFireToken, function(err, authData) {
-        if (err) {
-          throw err
-        } else {
-          callback(cardNumber, authData.username, addNewCard)
-        }
-    })
+function validateRoseFireToken(roseFireToken, cardNumber, callback) {
+    return function(callback) {
+        rosefire.verify(roseFireToken, function(err, authData) {
+            if (err) {
+                throw err
+            }
+            callback(null, cardNumber, authData.username)
+        })
+    }
 }
 
-function removeOldCard (cardNumber, username, callback) {
+function removeOldCard(cardNumber, username, callback) {
     client.hmget(USER_TO_CARD, username, (err, found) => {
         if (found) {
             client.hdel(CARD_TO_USER, found)
         }
-        callback(cardNumber, username, () => {
-            console.log('Made it, make func here')
-            return
-        })
+        callback(null, cardNumber, username)
     })
 }
 
-function addNewCard (cardNumber, username, callback) {
+function addNewCard(cardNumber, username, callback) {
+    console.log(cardNumber)
+    console.log(username)
     client.hmset(USER_TO_CARD, username, cardNumber, (err) => {
         client.hmset(CARD_TO_USER, cardNumber, username, callback)
     })
